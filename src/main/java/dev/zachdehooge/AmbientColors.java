924851baa38b42f736b6fe5b98ec5f8ddf90565a
zachdehooge <zacharydehooge@gmail.com>
2026-06-22 15:21:12 -0400

    Refactor color definitions in AmbientColors to use Color constructor

--- src/main/java/dev/zachdehooge/AmbientColors.java
+++ src/main/java/dev/zachdehooge/AmbientColors.java
@@ -7,8 +7,8 @@ public final class AmbientColors {
         throw new UnsupportedOperationException();
     }
 
-    public static final Color PDS_WARNING = Color.decode("ff00c9");
-    public static final Color WATCH = Color.decode("ffc900");
-    public static final Color WARNING = Color.decode("ff0000");
-    public static final Color SWS = Color.decode("0011FF");
+    public static final Color PDS_WARNING = new Color(0xff00c9);
+    public static final Color WATCH = new Color(0xffc900);
+    public static final Color WARNING = new Color(0xff0000);
+    public static final Color SWS = new Color(0x0011FF);
 }
