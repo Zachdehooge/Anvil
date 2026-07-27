0800fe0dd868026b16f1271f2c62962bfa392973
zachdehooge <zacharydehooge@gmail.com>
2026-07-27 11:59:32 -0400

    Add RadarSnippet class with methods for warning and image retrieval

---
+++ src/main/java/dev/zachdehooge/Utilities/RadarSnippet.java
@@ -0,0 +1,17 @@
+package dev.zachdehooge.Utilities;
+
+public class RadarSnippet {
+    /*
+        TODO: Need to get the radar data from IEM - https://mesonet.agron.iastate.edu/GIS/apps/rview/warnings.phtml - After I will need to query the alert somehow and get the images for the current warning and showing the composite at the time of issuance and the warning history for that storm.
+
+        Methods needed:
+            - getWarning
+            - getImage (pass the getWarning)
+
+        ?NOTE: Need to figure out how to pass the image to the embed
+    */
+
+    private static void getWarning(){}
+
+    private static void getImage(){}
+}
